b828a039e1e6a98a05c827c1c1d01b686e217e9f
DipperChen2007 <chenxingan65@gmail.com>
2025-12-15 14:22:35 -0800

    Insertion sort practice, first step of my coding

---
+++ Insertion_sort/decreasing_version.py
@@ -0,0 +1,11 @@
+arr = [2,1,4,19,20,14,10,99,24]
+for i in range(len(arr)):
+    key = arr[i]
+    j = i - 1 
+    while j >= 0 and arr[j] < key:
+        arr[j + 1] = arr[j]
+        j -= 1 
+    arr[j + 1] = key
+print(arr)
+
+
